5ae63796a58c8d59057adc86781cf2f290a606d6
Chandan Mahto <chandanmahto00761@gmail.com>
2021-12-03 22:07:23 +0530

    + changed python image in DockerFile to point to exact image. + added SWOUser as default User Model + added SWOUserManager

--- users/models.py
+++ users/models.py
@@ -1,11 +1,56 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models \
+    import AbstractBaseUser, PermissionsMixin, BaseUserManager
 
-# Create your models here.
+
+class SWOUserManager(BaseUserManager):
+    """
+    SWOUserManger is the default manager of SWOUser which is used
+    to create user objects.
+    """
+
+    def _create_user(self, email, password, **kwargs):
+        """
+        Creates a new user with provided email and password.
+        """
+
+        if not email:
+            raise ValueError("Email is required!")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **kwargs)
+        user.set_password(password)
+        user.save(using=self.db)
+        return user
+
+    def create_user(self, email, password=None, **kwargs):
+        kwargs.setdefault("is_superuser", False)
+        kwargs.setdefault("is_staff", False)
+        return self._create_user(email, password, **kwargs)
+
+    def create_superuser(self, email, password, **kwargs):
+        if not password:
+            raise ValueError("Password is required!")
+        kwargs.setdefault("is_superuser", True)
+        kwargs.setdefault("is_staff", True)
+        return self._create_user(email=email, password=password, **kwargs)
 
 
-# class SWOUser(AbstractUser):
-#     """
-#     SWOUser class is used to model users of the swodoc site.
-#     """
-#     pass
+class SWOUser(AbstractBaseUser, PermissionsMixin):
+    """
+    SWOUser is used to keep track of the users in the system.
+    """
+
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=512, null=False, blank=False)
+    last_name = models.CharField(max_length=512, null=False, blank=False)
+    is_staff = models.BooleanField(null=False, default=False)
+
+    objects = SWOUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = "user"
+        verbose_name_plural = "users"
+
--- users/tests/users_test.py
+++ users/tests/users_test.py
@@ -1,18 +1,51 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
-from users.models import SWOUser
+from users.models import SWOUser, SWOUserManager
 
 
 class UserTest(TestCase):
     """
     Test class related to users app.
     """
+
     def test_default_user_model(self):
         """
         Test if the user model used by the application is an instance
-        of SWOUser
-        :return:
+        of SWOUser i.e SWOUser is made the default User model for the
+        application.
         """
+
         user_model = get_user_model()
-        self.assertIsInstance(user_model, SWOUser)
+        self.assertIs(user_model, SWOUser)
+
+    def test_user_manager_set(self):
+        """
+        Tests if the user model is using the correct manager i.e.
+        whether SWOUser is using SWOUserManager
+        """
+
+        user_model = get_user_model()
+        self.assertIsInstance(user_model._default_manager, SWOUserManager)
+
+    def test_create_user(self):
+        """
+        Tests if users can be created from SWOUser
+        """
+
+        user_model = get_user_model()
+        user = user_model.objects.create_user(email="test@test.com")
+        self.assertIsInstance(user, SWOUser)
+        self.assertEqual(user.email, "test@test.com")
+
+    def test_create_superuser(self):
+        """
+        Tests if superusers can be created from SWOUser
+        """
+
+        user_model = get_user_model()
+        user = user_model.objects.create_superuser(email="admin@admin.com", password="pa88w07d@adm!n")
+        self.assertIsInstance(user, SWOUser)
+        self.assertTrue(user.is_superuser)
+        self.assertEqual(user.email, "admin@admin.com")
+        self.assertTrue(user.is_staff)
